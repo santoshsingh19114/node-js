a4e2a2d1f2bb3600dcf9637a5ed80c11adc710a0
santosh <sk1155610@gmail.com>
2024-01-11 16:49:06 +0530

    os module

---
+++ OsModule/osmod.js
@@ -0,0 +1,20 @@
+const os =require("os");
+
+console.log(os.arch());
+
+// const freememory = os.freemem();
+// console.log(freememory);
+
+// console.log(`${freememory/1024/1024/1024}`);
+
+
+
+// const totalmemory = os.totalmem();
+// console.log(`${totalmemory/1024/1024/1024}`);
+
+
+
+// console.log(os.hostname());
+// console.log(os.type()); 
+// console.log(os.platform());
+console.log(os.userInfo());
