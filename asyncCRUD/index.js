162bd7c5964ee8eeb3751731a0e921415b2b38dd
santosh <sk1155610@gmail.com>
2024-01-11 16:11:19 +0530

    async CRUD

---
+++ asyncCRUD/index.js
@@ -0,0 +1,31 @@
+const fs=require("fs");
+// fs.mkdir("santosh",(err)=>{
+//     console.log("folder created");
+// });
+// fs.writeFile('./bio.txt',"my name is santosh ",(err)=>{
+//     console.log("files created");
+// })
+
+// fs.appendFile("./bio.txt","Eat or Be Eaten",(err)=>{
+//     console.log("data appended");
+// });
+
+
+// fs.readFile("./bio.txt","utf-8",(err,data)=>{
+//     console.log(data);
+// });
+
+// fs.rename("./bio.txt","./mybio.txt",
+// (err)=>{
+//     console.log("renamed file");
+// });
+
+// fs.unlink("./mybio.txt",(err)=>{
+//     console.log("file deleted");
+//     console.log(err);
+// })
+
+
+// fs.rmdir("./santosh",(err)=>{
+//     console.log("folder deleted");
+// });
