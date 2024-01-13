3baf7465dedb034506d6a6d03343d4ffcb41bd84
santosh <sk1155610@gmail.com>
2024-01-13 15:21:05 +0530

    wrapperfunction

---
+++ modulewrapper/index.js
@@ -0,0 +1,10 @@
+(function(exports ,require,module,__filename,__dirname ){
+    const name ="santosh";
+//YE REQUIRE EXPORTS AUR MODULE ISI ME ADDJATE H ISILIYE YE PRIVATE MODULE JAISA KAAM KRTA H
+console.log(name); 
+    
+
+})();
+
+console.log(__dirname);
+    console.log(__filename);
